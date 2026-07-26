8190be2281ec4a9135b2ef65b08ae11017938dd4
Code-By-Aayush <cybertech.aayush@gmail.com>
2026-07-27 00:10:46 +0530

    Day 1: Print statements, strings, string concatenation, the input() function, variables, variable naming rules and Band Name Generator.

---
+++ Day 01/Day01Code.py
@@ -0,0 +1,36 @@
+# Write your code below this line 👇
+print("Hello World!")
+# Write your code below this line 👇
+print("Hello World!")
+print("Hello World!\nHello World!\nHello World!")
+print("Hello"+ " "+ "World"+"!")
+print("My name is" + " " + "Angela")
+car = input("What Car you Have? ")
+place = input ("What Place you Live? ")
+
+print(f"I have {car} car and I live in {place} ")
+
+print("Hello " + input("What is your Name? ") + "!")
+name = input("What is your name? ")
+print(name)
+
+Raju = 123456789
+Ravi = 987654321
+James = 123459876
+George = 567891234
+
+print(f"Phone Number of Raju is {Raju}")
+print(f"Phone Number of Ravi is {Ravi}")
+print(f"Phone Number of James is {James}")
+print(f"Phone Number of George is {George}")
+
+name = input("Type Your Name: ")
+name = name.replace(" ","")
+print(len(name))
+print("Welcome To The Band Name Generator !!!")
+user_city = input("What's the name of the city you grew up in?\n")
+user_pet_name = input("Whats your Pet's Name:\n")
+band_name = user_city +" "+user_pet_name+"!"
+print(f"Your Band Name is {band_name}")
+
+
